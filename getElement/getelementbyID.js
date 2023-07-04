e1292e1005d9cc62203a0705f042fa40f1514b31
ariasz <129909951+szarias@users.noreply.github.com>
2023-07-03 21:48:46 -0300

    Get Element By
    7

---
+++ getElement/getelementbyID.js
@@ -0,0 +1,14 @@
+let titulo = document.getElementById("title");
+
+titulo.innerHTML = "Ola Mundo novamente";
+
+let botao = document.getElementById("botao");
+
+//function clicar(){
+//    alert("O botão foi clicado");
+//}
+
+function clicar(mensagem){
+    alert(mensagem)
+}
+
---
+++ getElement/getelementbyID1.js
@@ -0,0 +1,7 @@
+let titulo = document.getElementById("title");
+
+titulo.innerHTML = "Clique aqui para saber o resultado da soma 10 + 10";
+
+function soma(x, y){
+    alert(x + y)
+}
